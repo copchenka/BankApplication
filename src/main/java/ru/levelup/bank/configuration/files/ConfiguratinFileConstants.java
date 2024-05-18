6eb4bea1e4f7240d72225e7ea1b4a64d27b4c051
Olesya Prokhorova <olesya.prokhorova@spbpu.com>
2024-05-18 10:27:47 +0300

    Change loading hibernate configuration

---
+++ src/main/java/ru/levelup/bank/configuration/files/ConfiguratinFileConstants.java
@@ -0,0 +1,9 @@
+package ru.levelup.bank.configuration.files;
+
+public final class ConfiguratinFileConstants {
+    private void ConfigurationFileConstants() {
+    }
+
+    public static final String CONFIGURATION_FILE_NAME = "src/main/resources/application.properties";
+
+}
